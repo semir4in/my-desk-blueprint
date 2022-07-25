3da64c1ae486cee0e6556fff80d386b38f2ad582
semir4in <semir4in@outlook.com>
2022-07-26 01:12:42 +0900

    Replace weather api: herokuapp ~ weatherdbi

--- src/weather.js
+++ src/weather.js
@@ -1,21 +1,27 @@
 function onGeoOkay(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+    const longitude = position.coords.longitude;
+    const latitude = position.coords.latitude;    
+    const url = `https://weatherdbi.herokuapp.com/data/weather/${latitude},${longitude}`;
     
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`;  
     
     fetch(url).then(response => response.json()).then(data => {
-        const cityName = data.name;
-        const weatherName = data.weather[0].main;
-        const tempature = data.main.temp;
+        const weatherSpan = document.getElementById("weather-a1");
 
-        const weatherSpan = document.getElementById("weather-span");
-        weatherSpan.innerText = `${cityName} with ${weatherName}, ${tempature}˚C`;
+        // const cityName = data.name;
+        // const weatherName = data.weather[0].main;
+        // const tempature = data.main.temp;
+        // weatherSpan.innerText = `${cityName} with ${weatherName}, ${tempature}˚C`;
+
+        const region = data.region;
+        const weatherComment = data.currentConditions.comment;
+        const tempature = data.currentConditions.temp.c;
+
+        weatherSpan.innerText = `${region} with ${weatherComment}, ${tempature}˚C`;
     });
 }
 
 function onGeoError() {
-    const weatherSpan = document.getElementById("weather-span");
-    weatherSpan.innerText = "unknown place. please give me your location";
+    alert("Can't find you. No weather for you");
 }
 
+navigator.geolocation.getCurrentPosition(onGeoOkay, onGeoError);
